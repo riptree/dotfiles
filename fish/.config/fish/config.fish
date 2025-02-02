ef1f2548fe922086287e2877f712a7097d5e593f
riptree <96670142+riptree@users.noreply.github.com>
2025-02-02 16:09:55 +0900

    update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,52 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
-end
-
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-# Environment variable
-
-# aliases
-alias vi="nvim"
-alias vim="nvim"
-alias view="nvim -R"
-alias dcd="docker compose down"
-alias dcu="docker compose up"
-alias a="cd .."
-alias aa="cd ../.."
-alias aaa="cd ../../.."
-alias aaaa="cd ../../../.."
-alias aaaaa="cd ../../../../.."
-alias g="git"
-alias gb="git branch"
-alias gs="git status"
-alias gsw="git switch"
-alias k="kubectl"
-
 # homebrew
 set PATH /opt/homebrew/bin $PATH
-
-# rbenv
-set -x PATH $HOME/.rbenv/bin $PATH
-status --is-interactive; and source (rbenv init -|psub)
-
-# pyenv
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PATH $PYENV_ROOT/bin
-pyenv init - | source
 
 # GOPATH
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
-set -x GOROOT (go1.21.5 env GOROOT)
+set -x GOROOT (go1.23.5 env GOROOT)
 set -x PATH $PATH $GOROOT/bin
 
 
@@ -64,3 +31,46 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 # GitHub CLI
 eval (gh completion -s fish | source)
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+function r
+    set repo_name (ghq list | fzf)
+
+    if test -n "$repo_name"
+        cd (ghq root)/$repo_name
+    end
+end
+
+function b
+  set branch (git branch --sort=-committerdate | fzf --reverse)
+  if test -n "$branch"
+    git checkout (echo $branch | sed 's/^[ *]*//')
+  end
+end
+
+# aliases
+alias vi="nvim"
+alias vim="nvim"
+alias view="nvim -R"
+alias dcd="docker compose down"
+alias dcu="docker compose up"
+alias dcr="docker compose restart"
+alias a="cd .."
+alias aa="cd ../.."
+alias aaa="cd ../../.."
+alias aaaa="cd ../../../.."
+alias aaaaa="cd ../../../../.."
+alias g="git"
+alias gb="git branch"
+alias gs="git status"
+alias gsw="git switch"
+alias k="kubectl"
+alias p="pnpm"
+
+if type -q eza
+  alias ll="eza -l -g --icons"
+  alias lla="ll -a"
+end
